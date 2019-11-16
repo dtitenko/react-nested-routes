c55c168a7f80cb65251d1ee208c4132b9ba00341
Dmitry Titenko <d.titenko@gmail.com>
2019-11-16 16:39:56 +0100

    Basic problem drafted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
+    <Router>
+      <Switch>
+        <Route path="/:lang" component={SubApp} />
+        <Redirect from="/" to="/en" />
+      </Switch>
+    </Router>
+  );
+}
+
+function SubApp({ match: { path } }) {
+  return (
+    <Switch>
+      <Route path={`${path}/:route`} component={RouteApp} />
+      <Redirect from="/" to="/en/route-1" />
+    </Switch>
+  );
+}
+
+function RouteApp({ match: { url, params: { lang, route } } }) {
+  return (
+    <div>
+      <h2>{lang} - {route}</h2>
+      <ul>
+        <li><Link to={`/${lang}/route-1`}>Route 1</Link></li>
+        <li><Link to={`/${lang}/route-2`}>Route 2</Link></li>
+        <li><Link to={`/${lang}/`}>Root</Link></li>
+      </ul>
     </div>
   );
 }
