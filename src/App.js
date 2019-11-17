6bf55a37d8fa46889da02a0c0b57a6bbf695d021
Dmitry Titenko <d.titenko@gmail.com>
2019-11-17 08:43:03 +0100

    Restore lang param in route app

--- src/App.js
+++ src/App.js
@@ -15,19 +15,19 @@ function App() {
   );
 }
 
-function SubApp({ match: { url }, location: { pathname, ...location } }) {
+function SubApp({ match: { url, params }, location: { pathname, ...location } }) {
   return (
-    <Switch location={{ ...location, pathname: pathname.slice(url.length) }}>
+    <Switch location={{ ...location, pathname: pathname.slice(url.length), prefix: url, params }}>
       <Route path={`/:route`} component={RouteApp} />
       <Redirect from="/" to="/route-1" />
     </Switch>
   );
 }
 
-function RouteApp({ match: { params: { route } } }) {
+function RouteApp({ match: { params: { route } }, location: { params: { lang } } }) {  
   return (
     <div>
-      <h2>? - {route}</h2>
+      <h2>{lang} - {route}</h2>
       <ul>
         <li><Link to={`/route-1`}>Route 1</Link></li>
         <li><Link to={`/route-2`}>Route 2</Link></li>
