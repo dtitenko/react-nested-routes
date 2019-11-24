5a15eebced33d4fb993ef7b22bbabb9818a0f06c
Dmitry Titenko <d.titenko@evision-software.com>
2019-11-24 19:08:00 +0100

    Apply prototype inheritance to simplify history module

--- src/history.js
+++ src/history.js
@@ -1,10 +1,8 @@
 import { createBrowserHistory } from "history";
 
-export function createHistory() {
-    const browserHistory = createBrowserHistory();
-
+function LanguageAwareHistory(history) {
     const getLangPrefix = () => {
-        const [, prefix] = /^(\/[a-z]{2})\/?/.exec(browserHistory.location.pathname) || ['', ''];
+        const [, prefix] = /^(\/[a-z]{2})\/?/.exec(history.location.pathname) || ['', ''];
         return prefix;
     };
 
@@ -26,24 +24,27 @@ export function createHistory() {
 
     // this history will restore lang prefix in the URL during SPA navigation
     // and href generation for anchor in `Link` component
-    const history = {
-        ...browserHistory,
-        get length() { return browserHistory.length; },
-        get action() { return browserHistory.action; },
-        get location() { return browserHistory.location; },
-        createHref: (href) => {
-            const prefix = getLangPrefix();
-            return browserHistory.createHref({ ...href, pathname: `${prefix}${href.pathname}` });
-        },
-        push: (origPath, state) => {
-            const path = applyLangPrefix(origPath);
-            return browserHistory.push(path, state);
-        },
-        replace: (origPath, state) => {
-            const path = applyLangPrefix(origPath);
-            return browserHistory.replace(path, state);
-        }
+    this.push = (origPath, state) => {
+        const path = applyLangPrefix(origPath);
+        return history.push(path, state);
     };
 
-    return history;
+    this.replace = (origPath, state) => {
+        const path = applyLangPrefix(origPath);
+        return history.replace(path, state);
+    };
+
+    this.createHref = (href) => {
+        const prefix = getLangPrefix();
+        return history.createHref({ ...href, pathname: `${prefix}${href.pathname}` });
+    };
+}
+
+export function createHistory() {
+    const browserHistory = createBrowserHistory();
+
+    LanguageAwareHistory.prototype = browserHistory;
+    LanguageAwareHistory.prototype.constructor = LanguageAwareHistory;
+
+    return new LanguageAwareHistory(browserHistory);
 }
